69516529a5b80e2d68b303b3b11aad9c21fda9d1
samuueel333 <samuelrodriguezc2008@gmail.com>
2026-08-04 17:33:49 +0200

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,1 +1,37 @@
+const texto = document.getElementById("texto");
 
+const mensajes = [
+    "30",
+    "31",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5 días...",
+    "Ha caducado."
+];
+
+let i = 0;
+
+function mostrar() {
+
+    texto.style.opacity = "1";
+    texto.innerHTML = mensajes[i];
+
+    i++;
+
+    if(i < mensajes.length){
+
+        setTimeout(() => {
+
+            texto.style.opacity = "0";
+
+            setTimeout(mostrar,300);
+
+        },1000);
+
+    }
+
+}
+
+mostrar();
